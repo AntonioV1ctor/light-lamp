cca99bf27b5269a48cd6d3ad6f407914c1c0e0e4
AntonioV1ctor <antoniovictor.me@gmail.com>
2025-05-04 17:35:42 -0400

    Correction

--- components/navbar.php
+++ components/navbar.php
@@ -1,6 +1,6 @@
 <header>
     <nav>
-       <img class="rasp-logo" src="/light-lamp/assets/images/icon_panel.png" alt="">
+       <img class="rasp-logo" src="assets/images/icon_panel.png" alt="">
        <h1>Raspbarry PI 3 panel</h1>
     </nav>
 </header>
