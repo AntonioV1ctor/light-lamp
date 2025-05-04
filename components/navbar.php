9ba3cd9d52c81fbd5308a04b838e208988defdf2
AntonioV1ctor <antoniovictor.me@gmail.com>
2025-05-04 17:17:50 -0400

    Fist Commit

---
+++ components/navbar.php
@@ -0,0 +1,6 @@
+<header>
+    <nav>
+       <img class="rasp-logo" src="/light-lamp/assets/images/icon_panel.png" alt="">
+       <h1>Raspbarry PI 3 panel</h1>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include_once "./components/head.php"?>
+
+<body>
+    <main class="main-home">
+        <div class="home-container">
+            <div class="home-box">
+            <?php include_once "./components/navbar.php"?>
+
+                <form class="login-user" action="#">
+                    <div>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
