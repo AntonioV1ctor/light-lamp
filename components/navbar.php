b57632b6fc5a0b836adad465a1e02e2a42a7a92a
AntonioV1ctor <antoniovictor.me@gmail.com>
2025-05-04 18:10:18 -0400

    New login panel v1

--- components/navbar.php
+++ components/navbar.php
@@ -1,6 +1,6 @@
 <header>
     <nav>
        <img class="rasp-logo" src="assets/images/icon_panel.png" alt="">
-       <h1>Raspbarry PI 3 panel</h1>
+       <h1>Raspberry PI 3 panel</h1>
     </nav>
 </header>
--- index.php
+++ index.php
@@ -1,14 +1,23 @@
-<?php include_once "./components/head.php"?>
+<?php include_once "./components/head.php" ?>
 
 <body>
     <main class="main-home">
         <div class="home-container">
             <div class="home-box">
-            <?php include_once "./components/navbar.php"?>
+                <?php include_once "./components/navbar.php" ?>
 
-                <form class="login-user" action="#">
-                    <div>
+                <form class="login-user-form" method="post" action="#">
+                    <div class="login-user-inputs">
+                        <div>
+                            <label for="email-login">Email</label>
+                            <input name="email-login" type="email">
+                        </div>
+                        <div>
+                            <label for="password-login">Password</label>
+                            <input name="password-login" type="password">
+                        </div>
 
+                        <input type="submit" value="Logar">
                     </div>
                 </form>
             </div>
